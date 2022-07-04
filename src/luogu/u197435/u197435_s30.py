21e44d701f94f82c22962bc7e757a1d2bf169a69
numberartificial <c05118006@vip.qq.com>
2022-07-04 09:59:05 +0800

    197435 fix time over; 得分 30

--- src/luogu/u197435/u197435_s30.py
+++ src/luogu/u197435/u197435_s30.py
@@ -7,6 +7,8 @@
 # 时间复杂度: O((N / 2)! * (N/2)!)
 # 本地测 N = 12 需要 0.7S, python还是比较慢
 
+# 得分 30
+
 import sys
 import os
 import time
@@ -25,13 +27,22 @@ def swap(a, i, j):
 
 def dfs(a, dep, k):
     if (dep == k):
-        all_p.append(' '.join(map(str, a)))
-        all_p2.append(' '.join(list(map(lambda x:str(x + k), a))))
+        all_p.append(a[:])
+        all_p2.append(list(map(lambda x:x + k, a)))
 
     for i in range(dep, len(a)):
         swap(a, dep, i)
         dfs(a, dep + 1, k)
         swap(a, dep, i)
+
+def print_line(a, b, k):
+    print(' '.join(map(str, a)), end=' ')
+    print(' '.join(map(str, b)))
+    # for x in a:
+    #     print(x, end=' ')
+    # for x in b:
+    #     print(x + k, end=' ')
+    # print()
 
 def cal(n):
     k = int(n / 2)
@@ -41,7 +52,7 @@ def cal(n):
     dfs(a, 0, k)
     for i in range(0, len(all_p)):
         for j in range(0, len(all_p2)):
-           print(all_p[i] + ' ' + all_p2[j]) 
+           print_line(all_p[i], all_p2[j], k) 
 
 def main():
     n = int(input())
